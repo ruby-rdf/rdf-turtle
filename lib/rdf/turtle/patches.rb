103fd18401291141a253268d0c72d233463ed657
Gregg Kellogg <gregg@kellogg-assoc.com>
2011-08-26 22:01:43 -0700

    Remove RDF::Literal::String, for now.

--- lib/rdf/turtle/patches.rb
+++ lib/rdf/turtle/patches.rb
@@ -1,12 +1,5 @@
 require 'rdf'
 
-class RDF::Literal::String
-  ##
-  # RDF 1.1 and Turtle define that all plain literals are the same as having datatype xsd:string.
-  # However, the xsd:string should not be serialized
-  def has_datatype?; false; end
-end
-
 module RDF
   class List
     ##
