8c4e80cf2208c2e7c072205dbcae832b8eb3a4e5
Gregg Kellogg <gregg@kellogg-assoc.com>
2013-03-24 12:44:37 -0700

    Freeze terminal constants.

--- lib/rdf/turtle/terminals.rb
+++ lib/rdf/turtle/terminals.rb
@@ -13,8 +13,8 @@ module RDF::Turtle
                            [\\u2070-\\u218F]|[\\u2C00-\\u2FEF]|[\\u3001-\\uD7FF]|
                            [\\uF900-\\uFDCF]|[\\uFDF0-\\uFFFD]|[\\u{10000}-\\u{EFFFF}]
                          EOS
-      U_CHARS2         = Regexp.compile("\\u00B7|[\\u0300-\\u036F]|[\\u203F-\\u2040]")
-      IRI_RANGE        = Regexp.compile("[[^<>\"{}|^`\\\\]&&[^\\x00-\\x20]]")
+      U_CHARS2         = Regexp.compile("\\u00B7|[\\u0300-\\u036F]|[\\u203F-\\u2040]").freeze
+      IRI_RANGE        = Regexp.compile("[[^<>\"{}|^`\\\\]&&[^\\x00-\\x20]]").freeze
     else
       ##
       # UTF-8 regular expressions for Ruby 1.8.x.
@@ -62,60 +62,60 @@ module RDF::Turtle
     # 26
     UCHAR                = EBNF::LL1::Lexer::UCHAR
     # 170s
-    PERCENT              = /%[0-9A-Fa-f]{2}/
+    PERCENT              = /%[0-9A-Fa-f]{2}/.freeze
     # 172s
-    PN_LOCAL_ESC         = /\\[_~\.\-\!$\&'\(\)\*\+,;=:\/\?\#@%]/
+    PN_LOCAL_ESC         = /\\[_~\.\-\!$\&'\(\)\*\+,;=:\/\?\#@%]/.freeze
     # 169s
-    PLX                  = /#{PERCENT}|#{PN_LOCAL_ESC}/
+    PLX                  = /#{PERCENT}|#{PN_LOCAL_ESC}/.freeze.freeze
     # 163s
-    PN_CHARS_BASE        = /[A-Z]|[a-z]|#{U_CHARS1}/
+    PN_CHARS_BASE        = /[A-Z]|[a-z]|#{U_CHARS1}/.freeze
     # 164s
-    PN_CHARS_U           = /_|#{PN_CHARS_BASE}/
+    PN_CHARS_U           = /_|#{PN_CHARS_BASE}/.freeze
     # 166s
-    PN_CHARS             = /-|[0-9]|#{PN_CHARS_U}|#{U_CHARS2}/
-    PN_LOCAL_BODY        = /(?:(?:\.|:|#{PN_CHARS}|#{PLX})*(?:#{PN_CHARS}|:|#{PLX}))?/
-    PN_CHARS_BODY        = /(?:(?:\.|#{PN_CHARS})*#{PN_CHARS})?/
+    PN_CHARS             = /-|[0-9]|#{PN_CHARS_U}|#{U_CHARS2}/.freeze
+    PN_LOCAL_BODY        = /(?:(?:\.|:|#{PN_CHARS}|#{PLX})*(?:#{PN_CHARS}|:|#{PLX}))?/.freeze
+    PN_CHARS_BODY        = /(?:(?:\.|#{PN_CHARS})*#{PN_CHARS})?/.freeze
     # 167s
-    PN_PREFIX            = /#{PN_CHARS_BASE}#{PN_CHARS_BODY}/
-    # 100s
-    PN_LOCAL             = /(?:[0-9]|:|#{PN_CHARS_U}|#{PLX})#{PN_LOCAL_BODY}/
+    PN_PREFIX            = /#{PN_CHARS_BASE}#{PN_CHARS_BODY}/.freeze
+    # 168s
+    PN_LOCAL             = /(?:[0-9]|:|#{PN_CHARS_U}|#{PLX})#{PN_LOCAL_BODY}/.freeze
     # 154s
     EXPONENT             = /[eE][+-]?[0-9]+/
     # 159s
     ECHAR                = /\\[tbnrf\\"']/
     # 18
-    IRIREF               = /<(?:#{IRI_RANGE}|#{UCHAR})*>/
+    IRIREF               = /<(?:#{IRI_RANGE}|#{UCHAR})*>/.freeze
     # 139s
-    PNAME_NS             = /#{PN_PREFIX}?:/
+    PNAME_NS             = /#{PN_PREFIX}?:/.freeze
     # 140s
-    PNAME_LN             = /#{PNAME_NS}#{PN_LOCAL}/
+    PNAME_LN             = /#{PNAME_NS}#{PN_LOCAL}/.freeze
     # 141s
-    BLANK_NODE_LABEL     = /_:(?:[0-9]|#{PN_CHARS_U})((#{PN_CHARS}|\.)*#{PN_CHARS})?/
+    BLANK_NODE_LABEL     = /_:(?:[0-9]|#{PN_CHARS_U})(?:(?:#{PN_CHARS}|\.)*#{PN_CHARS})?/.freeze
     # 144s
-    LANGTAG              = /@[a-zA-Z]+(?:-[a-zA-Z0-9]+)*/
+    LANGTAG              = /@[a-zA-Z]+(?:-[a-zA-Z0-9]+)*/.freeze
     # 19
-    INTEGER              = /[+-]?[0-9]+/
+    INTEGER              = /[+-]?[0-9]+/.freeze
     # 20
-    DECIMAL              = /[+-]?(?:[0-9]*\.[0-9]+)/
+    DECIMAL              = /[+-]?(?:[0-9]*\.[0-9]+)/.freeze
     # 21
-    DOUBLE               = /[+-]?(?:[0-9]+\.[0-9]*#{EXPONENT}|\.?[0-9]+#{EXPONENT})/
+    DOUBLE               = /[+-]?(?:[0-9]+\.[0-9]*#{EXPONENT}|\.?[0-9]+#{EXPONENT})/.freeze
     # 22
-    STRING_LITERAL_QUOTE      = /'(?:[^\'\\\n\r]|#{ECHAR}|#{UCHAR})*'/
+    STRING_LITERAL_QUOTE      = /'(?:[^\'\\\n\r]|#{ECHAR}|#{UCHAR})*'/.freeze
     # 23
-    STRING_LITERAL_SINGLE_QUOTE      = /"(?:[^\"\\\n\r]|#{ECHAR}|#{UCHAR})*"/
+    STRING_LITERAL_SINGLE_QUOTE      = /"(?:[^\"\\\n\r]|#{ECHAR}|#{UCHAR})*"/.freeze.freeze
     # 24
-    STRING_LITERAL_LONG_SINGLE_QUOTE = /'''(?:(?:'|'')?(?:[^'\\]|#{ECHAR}|#{UCHAR}))*'''/m
+    STRING_LITERAL_LONG_SINGLE_QUOTE = /'''(?:(?:'|'')?(?:[^'\\]|#{ECHAR}|#{UCHAR}))*'''/m.freeze
     # 25
-    STRING_LITERAL_LONG_QUOTE = /"""(?:(?:"|"")?(?:[^"\\]|#{ECHAR}|#{UCHAR}))*"""/m
+    STRING_LITERAL_LONG_QUOTE = /"""(?:(?:"|"")?(?:[^"\\]|#{ECHAR}|#{UCHAR}))*"""/m.freeze
 
     # 161s
-    WS                   = / |\t|\r|\n  /
+    WS                   = / |\t|\r|\n  /.freeze
     # 162s
-    ANON                 = /\[#{WS}*\]/
+    ANON                 = /\[#{WS}*\]/m.freeze
     # 28t
-    SPARQL_PREFIX        = /prefix/i
+    SPARQL_PREFIX        = /prefix/i.freeze
     # 29t
-    SPARQL_BASE          = /base/i
+    SPARQL_BASE          = /base/i.freeze
   
   end
 end
